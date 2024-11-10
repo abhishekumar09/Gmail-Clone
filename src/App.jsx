51c0a10841556daf43525573042ed73c54c5bd06
abhishekumar09 <2003abhishek.09@gmail.com>
2024-11-10 22:19:57 +0530

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import Navbar from './components/shared/Navbar'
 
 export default function App() {
   return (
     <h1 className="text-3xl font-bold text-center text-red-600 underline">
       Hello world!
+      </Navbar>
     </h1>
   )
 }
--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,3 +1,16 @@
 import React from "react";
 import { RxHamburgerMenu } from "react-icons/rx";
 
+
+
+const Navbar = () => {
+  return (
+    <div>
+      <RxHamburgerMenu />
+
+
+    </div>
+  )
+}
+
+export default Navbar
